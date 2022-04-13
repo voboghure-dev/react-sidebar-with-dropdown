38b40d94b38d9e804c0900e829e68189c361f45f
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-04-13 14:50:34 +0600

    Create sidebar
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Sidebar />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { SidebarData } from './SidebarData';
+
+const Nav = styled.div`
+  background: #15171c;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const NavIcon = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const SidebarNav = styled.nav`
+  background: #15171c;
+  width: 250px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
+  transition: 350ms;
+  z-index: 10;
+`;
+
+const SidebarWrap = styled.div`
+  width: 100%;
+`;
 
 export default function Sidebar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
-    <div>Sidebar</div>
-  )
+    <>
+      <Nav>
+        <NavIcon to='#'>
+          <FaIcons.FaBars onClick={showSidebar} />
+        </NavIcon>
+      </Nav>
+      <SidebarNav sidebar={sidebar}>
+        <SidebarWrap>
+          <NavIcon to='#'>
+            <AiIcons.AiOutlineClose onClick={showSidebar} />
+          </NavIcon>
+        </SidebarWrap>
+      </SidebarNav>
+    </>
+  );
 }
