0d3a58de6c9d0535d5b778cae0d0da9bd34d3705
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-04-13 13:35:07 +0600

    Add components skeleton and install packages
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      Yoo
-    </div>
+    <>
+      <Sidebar />
+    </>
   );
 }
 
